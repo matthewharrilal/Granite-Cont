cc400f8268e8d18ff177c093c3ef9c3aa7525adb
Matthew Harrilal <matthewharrilal@gmail.com>
2019-04-17 10:17:48 -0700

    Able to load animation -> have to set as root view

--- Granite(Revisited)/Controllers/LaunchScreenController.swift
+++ Granite(Revisited)/Controllers/LaunchScreenController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import Lottie
 
 class LaunchScreenViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        configureAnimation()
     }
-
+    
+    
+    private func configureAnimation() {
+        
+        let animationView = LOTAnimationView(name: "launchAnimation")
+        animationView.contentMode = .scaleAspectFit
+        
+        self.view.backgroundColor = .clear
+        
+        animationView.loopAnimation = true
+        self.view.addSubview(animationView)
+        
+        animationView.frame = self.view.bounds
+        
+        animationView.play { (completed) in
+            print("Has completed animation? -> \(completed)")
+        }
+    }
 
 }
 
--- Granite(Revisited)/Views/LaunchScreenView.swift
+++ Granite(Revisited)/Views/LaunchScreenView.swift
@@ -8,15 +8,34 @@
 
 import Foundation
 import UIKit
+import Lottie
 
 class LaunchScreenView: UIView {
     
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configureAnimation()
+        print("Entering here")
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        configureAnimation()
+    }
+    
+    private func configureAnimation() {
+        Bundle.main.loadNibNamed("LaunchScreenView", owner: self, options: nil)
+        
+        let animationView = LOTAnimationView(name: "launchAnimation")
+        animationView.contentMode = .center
+        
+        animationView.loopAnimation = true
+        self.addSubview(animationView)
+        
+        animationView.frame = self.bounds
+        
+        animationView.play { (completed) in
+            print("Has completed animation? -> \(completed)")
+        }
     }
 }
