97466990ad1d9fc748fc0e180a7e2aea8781a665
Matthew Harrilal <matthewharrilal@gmail.com>
2019-04-17 09:18:22 -0700

    Initial Commit of the Revisited Granite Project

---
+++ Granite(Revisited)/Controllers/LaunchScreenController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Granite(Revisited)
+//
+//  Created by Matthew Harrilal on 4/17/19.
+//  Copyright © 2019 Matthew Harrilal. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Granite(Revisited)/Views/LaunchScreenView.swift
@@ -0,0 +1,22 @@
+//
+//  LaunchScreenView.swift
+//  Granite(Revisited)
+//
+//  Created by Matthew Harrilal on 4/17/19.
+//  Copyright © 2019 Matthew Harrilal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LaunchScreenView: UIView {
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
