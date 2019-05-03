d4c43bff0b6dfa2cb3fed340a63505dbf88661c4
Matthew Harrilal <matthewharrilal@gmail.com>
2019-05-02 19:38:40 -0700

    In the process of implementing manager to contain network execution

---
+++ Granite(Revisited)/NetworkingLayer/Manager/NetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkManager.swift
+//  Granite(Revisited)
+//
+//  Created by Matthew Harrilal on 5/2/19.
+//  Copyright © 2019 Matthew Harrilal. All rights reserved.
+//
+
+import Foundation
