390ba8b3e716fc02418a28006f4c02209de6a2d2
Matthew Harrilal <matthewharrilal@gmail.com>
2019-05-02 19:45:59 -0700

    Added error descriptions for easy throw statements and in the process of implementing method to retrieve user

--- Granite(Revisited)/NetworkingLayer/Manager/NetworkManager.swift
+++ Granite(Revisited)/NetworkingLayer/Manager/NetworkManager.swift
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+
+typealias UserCompletion = (_ user: User?, _ response: URLResponse?, _ error: Error?) -> ()
+
+public struct NetworkManager {
+    // In charge of containing our routers for each endpoint
+    let userAccess = Router<UserAccess>()
+}
+
+
+
+// Some elegant enums used to be able to mark the status of the request at every step of the way such as the decoding process, the network request process, and others that you can see below
+public enum NetworkResponse: String {
+    // Possible error scenarios provides an abstracted and modularized form of error handling
+    case success = "Network call was a success"
+    case authenticationError = "You need to be authenticated first"
+    case badRequest = "Bad request"
+    case outdated = "The url you requested is outdated"
+    case failed = "The network call resulted in a failure"
+    case noData = "The network call resulted in no data being sent back"
+    case unableToDecode = "Unable to decode the response"
+}
+
+public enum Result<String> {
+    case success(String)
+    case failure(String)
+}
+
+
+
+fileprivate func handleNetworkResponse(_ response: HTTPURLResponse) -> Result<String> {
+    // Depending on the status code we can infer the origin of the error
+    switch response.statusCode {
+    case 200 ... 299:
+        return .success(NetworkResponse.success.rawValue)
+    case 401 ... 500:
+        return .failure(NetworkResponse.authenticationError.rawValue)
+    case 501 ... 599:
+        return .failure(NetworkResponse.badRequest.rawValue)
+    case 600:
+        return .failure(NetworkResponse.outdated.rawValue)
+    default:
+        return .failure(NetworkResponse.failed.rawValue)
+        
+    }
+}
