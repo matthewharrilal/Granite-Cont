2b45f5d0c80dfebb631959b98489450b528f7c28
Matthew Harrilal <matthewharrilal@gmail.com>
2019-05-02 19:35:11 -0700

    Made an enum to store different REST Http Methods

---
+++ Granite(Revisited)/Helpers/HTTPMethods.swift
@@ -0,0 +1,19 @@
+//
+//  HTTPMethods.swift
+//  Granite(Revisited)
+//
+//  Created by Matthew Harrilal on 5/2/19.
+//  Copyright © 2019 Matthew Harrilal. All rights reserved.
+//
+
+import Foundation
+
+public enum HTTPMethods: String {
+    case get = "GET"
+    
+    case post = "POST"
+    
+    case delete = "DELETE"
+    
+    case put = "PUT"
+}
