d9bf95314c54999cb713bf5b37371ecc0a9efc0d
Matthew Harrilal <matthewharrilal@gmail.com>
2019-05-02 19:39:36 -0700

    Made choice of url parameters in type of request parameter

--- Granite(Revisited)/NetworkingLayer/Service/HTTPTask.swift
+++ Granite(Revisited)/NetworkingLayer/Service/HTTPTask.swift
@@ -14,7 +14,7 @@ public typealias Parameters = [String: Any]
 public enum HTTPTask {
     case request
     
-    case requestParameters(bodyParameters: Parameters?, urlParameters: Parameters)
+    case requestParameters(bodyParameters: Parameters?, urlParameters: Parameters?)
     
     case requestParametersAndHeaders(bodyParameters: Parameters?, urlParameters: Parameters?, headers: HTTPHeaders?)
 }
