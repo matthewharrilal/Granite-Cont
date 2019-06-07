e01d9b3ff8432859ee580306eb9c8d1095d2eeda
Matthew Harrilal <matthewharrilal@gmail.com>
2019-06-07 11:01:45 -0700

    Animating inner and outer container view of the signup view

--- Granite(Revisited)/Views/SignUpView.swift
+++ Granite(Revisited)/Views/SignUpView.swift
@@ -15,6 +15,7 @@ class SignUpView: UIView {
     @IBOutlet weak var createAccountView: UIView!
     @IBOutlet weak var transitionView: UIView!
     var textFieldView: UIView!
+    var outerContainerView: UIView!
     var innerContainerView: UIView!
     
     override init(frame: CGRect) {
@@ -22,6 +23,8 @@ class SignUpView: UIView {
         commonInit()
         
         animateInnerContainerView()
+        
+        animateCreateAccountView()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -33,21 +36,16 @@ class SignUpView: UIView {
         addSubviews(views: animationContainerView, createAccountView)
         self.animationContainerView.frame = .init(x: self.bounds.minX, y: self.bounds.minY, width: self.bounds.width, height: self.bounds.height / 3)
         
-        self.createAccountView.frame = .init(x: self.bounds.midX, y: createAccountView.frame.origin.y, width: self.bounds.width / 1.3, height: self.bounds.height / 2)
+        self.createAccountView.frame = .init(x: self.bounds.midX, y: createAccountView.bounds.origin.y, width: -self.bounds.width, height: self.bounds.height / 2)
         
-        self.createAccountView.center = self.center
         
         self.backgroundColor = .init(hexString: "d5b9b2")
         
-        self.createAccountView.anchor(top: self.animationContainerView.bottomAnchor, leading: self.safeAreaLayoutGuide.leadingAnchor, bottom: self.safeAreaLayoutGuide.bottomAnchor, trailing: self.safeAreaLayoutGuide.trailingAnchor, padding: .init(top: 10, left: -50, bottom: 50, right: -20))
         
         loadAnimationView()
         
-        //        self.animationContainerView.backgroundColor = .red
-        
         self.createAccountView.backgroundColor = .blue
         
-        //        self.transitionView.backgroundColor = .orange
         
         createInnerContainerView()
     }
@@ -70,25 +68,30 @@ class SignUpView: UIView {
         self.innerContainerView = UIView(frame: .init(x: self.createAccountView.bounds.origin.x, y: self.createAccountView.bounds.origin.y, width: -self.bounds.width, height: self.createAccountView.bounds.height))
         innerContainerView.backgroundColor = .white
         
-        self.createAccountView.addSubview(innerContainerView)
-        
-        self.createAccountView.anchor(top: self.animationContainerView.bottomAnchor, leading: self.leadingAnchor, bottom: innerContainerView.topAnchor, trailing: self.trailingAnchor, padding: .init(top: 10, left: -50, bottom: 10, right: 0 ))
-        
+        self.addSubview(innerContainerView)
         
         
         innerContainerView.layer.cornerRadius = 20
         
         
         
-        //        innerContainerView.bounds.width -= self.bounds.width
     }
     
     func animateInnerContainerView() {
-        UIView.animate(withDuration: 1.0) {
-            self.innerContainerView.frame = .init(x: self.createAccountView.bounds.origin.x, y: self.createAccountView.bounds.origin.y, width: self.bounds.width, height: self.createAccountView.bounds.height)
-        }
         
-        innerContainerView.anchor(top: self.createAccountView.topAnchor, leading: self.createAccountView.leadingAnchor, bottom: self.createAccountView.bottomAnchor, trailing: self.createAccountView.trailingAnchor, padding: .init(top: 30, left: -50, bottom: 0, right: -20))
+        UIView.animate(withDuration: 2.0, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.1, options: .curveEaseInOut, animations: {
+            self.innerContainerView.frame = .init(x: self.createAccountView.bounds.origin.x, y: self.createAccountView.bounds.origin.y, width: self.bounds.width, height: self.createAccountView.bounds.height)
+        }, completion: nil)
+        
+        self.innerContainerView.anchor(top: self.createAccountView.topAnchor, leading: self.leadingAnchor, bottom: self.bottomAnchor, trailing: self.trailingAnchor, padding: .init(top: 30, left: -50, bottom: 0, right: -70))
+    }
+    
+    func animateCreateAccountView() {
+        UIView.animate(withDuration: 2.0, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.1, options: .curveEaseInOut, animations: {
+            self.createAccountView.frame = .init(x: self.createAccountView.bounds.midX, y: self.createAccountView.bounds.origin.y, width: self.bounds.width, height: self.createAccountView.bounds.height)
+        }, completion: nil)
+        
+        self.createAccountView.anchor(top: self.animationContainerView.bottomAnchor, leading: self.leadingAnchor, bottom: self.bottomAnchor, trailing: self.trailingAnchor, padding: .init(top: 10, left: -50, bottom: 50, right: -20))
     }
     
     //    func createCustomTextField() {
