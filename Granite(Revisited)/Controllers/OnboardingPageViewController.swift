abf83d566421d8e4202a46e619f473854e05f42f
Matthew Harrilal <matthewharrilal@gmail.com>
2019-08-05 07:22:42 -0700

    Working on the use of the onboarding page view controller

--- Granite(Revisited)/Controllers/OnboardingPageViewController.swift
+++ Granite(Revisited)/Controllers/OnboardingPageViewController.swift
@@ -11,11 +11,45 @@ import UIKit
 
 class OnboardingPageViewController: UIPageViewController {
     
+    private lazy var orderedViewControllers: [UIViewController] = [OnboardingController(), SecondOnboardingController()]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.dataSource = self
-        self.delegate = self
+        
+        if let firstViewController = self.orderedViewControllers.first {
+            setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+        }
     }
     
+    override init(transitionStyle style: UIPageViewController.TransitionStyle, navigationOrientation: UIPageViewController.NavigationOrientation, options: [UIPageViewController.OptionsKey : Any]? = nil) {
+        super.init(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
+
+
+extension OnboardingPageViewController: UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = self.orderedViewControllers.firstIndex(of: viewController) else {return nil}
+        
+        
+        let previousIndex = viewControllerIndex - 1
+        guard previousIndex >= 0 else {
+            return orderedViewControllers.last // Loop the ordered view controllers
+        }
+        
+        guard self.orderedViewControllers.count > previousIndex else {return nil}
+        
+        
+        return self.orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        return nil
+    }
     
 }
